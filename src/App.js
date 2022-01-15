2c0f50ca714a3a57aeaa84b27115129ec4a58eb9
Sabine Tait <sabine.tait@gmail.com>
2022-01-15 14:50:04 -0500

    inital commit :rocket:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Stars Align</h1>
       </header>
+
+      <main>
+        <div>
+          <h2>Select your astrological sign for your daily horoscope</h2>
+
+          <section className="astro-buttons">
+            <button>Aries</button>
+            <button>Taurus</button>
+            <button>Gemini</button>
+            <button>Cancer</button>
+            <button>Leo</button>
+            <button>Virgo</button>
+            <button>Libra</button>
+            <button>Scorpio</button>
+            <button>Sagittarius</button>
+            <button>Capricorn</button>
+            <button>Aquarius</button>
+            <button>Pisces</button>
+          </section>
+
+          <section className="user-horoscopeReading">
+            <h2>Date:</h2>
+
+            <div className="text-content">
+
+              <div class="daily-reading">
+                <p></p>
+              </div>
+
+              <div class="sign-characteristics">
+                <p>Colour:</p>
+                <p>Compatibility:</p>
+                <p>Lucky number:</p>
+                <p>Mood:</p>
+                <p>Time to thrive:</p>
+              </div>
+              
+            </div>
+          </section>
+      
+        </div>
+      </main>
+      <footer>
+        <p>Create at Juno College 2022</p>
+      </footer>
     </div>
   );
 }
